26b4916a1fc71b9af508229b6c201535a0aeeb69
Chinmoy <chinmoy.dehingia@gmail.com>
2022-12-18 16:16:53 +0530

    Route Testing

--- Backend/app.js
+++ Backend/app.js
@@ -2,14 +2,14 @@
 const express = require("express");
 const dotenv = require("dotenv")
 const cors= require("cors")
-
+const route  = require("./Routes/route")
 //Use and config
 const app=express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-
+app.use("/",route)
 
 
 app.listen(process.env.PORT,()=>{
