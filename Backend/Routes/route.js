b2325050cd8fc38607da1ff8e5d7b75ddd5a8105
Chinmoy <chinmoy.dehingia@gmail.com>
2022-12-20 20:56:14 +0530

    uploading the csv and putting in the the database

--- Backend/Routes/route.js
+++ Backend/Routes/route.js
@@ -1,8 +1,39 @@
 const router = require("express").Router();
-
+const csvtojson =require("csvtojson");
+const upload = require("../Middlewear/middle")
+const contact= require("../Models/contacts")
+const fs = require("fs");
 
 router.get("/",(req,res)=>{
     res.send("Working");
 })
+
+//Inserting the csv
+router.post("/upload",upload.single("contact"),(req,res)=>{
+    try{
+       
+        csvtojson().fromFile("public/contact.csv")
+        .then(async (csvData)=>{
+            // console.log(csvData)
+            await contact.insertMany(csvData)
+            .then(async ()=>{
+                await fs.unlink("public/contact.csv",(err)=>{console.log(err)})
+                res.json({
+                    message:"sucessfully inserted"
+                })
+            }).catch((e)=>{
+                res.json({
+                    message:e.message
+                })
+            })
+        })
+    }catch(e){
+        res.status(400).json({
+            message:e.message
+        })
+    }
+})
+
+//Deleting from csv
 
 module.exports= router;
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,9 @@
-
+import Export from "./components/Export";
 
 function App() {
   return (
     <div className="App">
-
-     
+      <Export/>
     </div>
   );
 }
