ca922432fa439dd03781a32b708eebf308096b28
Shweta <shwetaghanwat5@gmail.com>
2022-12-21 11:56:55 +0530

    removed errors and modify app file

--- Backend/app.js
+++ Backend/app.js
@@ -4,14 +4,12 @@ const dotenv = require("dotenv")
 const cors= require("cors")
 const route  = require("./Routes/route")
 const connectDB=require("./connectDB/connect");
-<<<<<<< HEAD
+
 const login = require("./Routes/login")
 
 
-=======
 const getContacts=require("./Routes/getContacts")
 const searchContacts=require("./Routes/search")
->>>>>>> c0697d5a24e195d699f04c75d41c9dcbe6f29543
 
 //Use and config
 const app=express();
@@ -19,14 +17,11 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-<<<<<<< HEAD
 app.use("/",route);
 app.use("/",login)
-=======
 app.use("/",route)
 app.use("/getContacts",getContacts);
 app.use("/search",searchContacts);
->>>>>>> c0697d5a24e195d699f04c75d41c9dcbe6f29543
 
 
 app.listen(process.env.PORT,async ()=>{
