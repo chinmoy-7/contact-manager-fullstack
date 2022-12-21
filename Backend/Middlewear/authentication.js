eeeac0f29c69ad37937ccc6fe3a9260d5343611d
Shweta <shwetaghanwat5@gmail.com>
2022-12-21 11:49:09 +0530

    login / register api is added and middleware is added

---
+++ Backend/Middlewear/authentication.js
@@ -0,0 +1,32 @@
+var jwt=require('jsonwebtoken');
+const { JWT_SECRET } = require("../keys")
+const express = require("express");
+const router = express.Router();
+router.use("/", async (req, res, next) => {
+
+    if(req.headers.authorization){
+        const token=req.headers.authorization;
+        console.log(token)
+        if(token){
+            jwt.verify(token,JWT_SECRET,async(err,decoded)=>{
+                if(err){
+                    return res.status(403).json({
+                        status:"failed",
+                        message:"not a valid token"
+                    })
+                }
+                // console.log(decoded);
+                req.user=decoded.user_id;
+                next()
+            })
+        }
+    }else{
+        return res.status(403).json({
+            status:"failed",
+            message:"unauthorized"
+        })
+    }}
+)
+
+
+module.exports=router
--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const dotenv = require("dotenv")
 const cors= require("cors")
 const route  = require("./Routes/route")
-const connectDB=require("./connectDB/connect")
+const connectDB=require("./connectDB/connect");
+const login = require("./Routes/login")
+
 
 
 //Use and config
@@ -12,7 +14,8 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-app.use("/",route)
+app.use("/",route);
+app.use("/",login)
 
 
 app.listen(process.env.PORT,async ()=>{
