217ad1b5c2de65792c8d50753c6fd1b192caa79d
AshwiniReddy99999 <ashwinisreddy12@gmail.com>
2022-12-21 13:40:56 +0530

    modified search

--- Backend/Routes/getContacts.js
+++ Backend/Routes/getContacts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const Contacts=require("../Models/contacts")
+const Contacts=require("../Models/contacts");
+
 
 router.get("/",async(req,res)=>{
     try{
--- Backend/Routes/search.js
+++ Backend/Routes/search.js
@@ -1,14 +1,13 @@
 const router = require("express").Router();
 const Contacts=require("../Models/contacts")
+const Auth=require("../Middlewear/authentication")
 
 router.get("/:email",async(req,res)=>{
     try{
-        const Contact=await Contacts.findOne(req.params.email);
-        if(Contact){
-          if(Contact[0].user==req.user){
+        const Contact=await Contacts.findOne({email:req.params.email});
+        
+          
             res.status(200).json(Contact);
-          }
-        }
         
     }catch{
             res.status(404).json("email not found");
