d9b63d8ca792653679e1dd5b367fe08096b75b51
Shweta <shwetaghanwat5@gmail.com>
2022-12-22 18:40:35 +0530

    sign in and sign up pageis added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,15 @@
 import Export from "./components/Export";
 import TotalContact from "./pages/TotalContacts";
+import Signin from './pages/signInPage/signin';
+import Signup from "./pages/signUpPage/signup";
 function App() {
   return (
     <div className="App">
-      <Export/>
-      <TotalContact/>
+      {/* <Export/>
+      <TotalContact/> */}
+      {/* <Login/> */}
+      {/* <Signin /> */}
+    <Signup/>
     </div>
   );
 }
