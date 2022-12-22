c793799fd05470dc959f3bc16a0e10e21c44d928
Shweta <shwetaghanwat5@gmail.com>
2022-12-22 19:42:09 +0530

    modified signup page as adding confirm password input

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,31 +1,20 @@
-import Export from "./components/Export";
-import TotalContact from "./pages/TotalContacts";
-import Signin from './pages/signInPage/signin';
+// import Export from "./components/Export";
+// import TotalContact from "./pages/TotalContacts";
+// import Signin from './pages/signInPage/signin';
 import Signup from "./pages/signUpPage/signup";
+// import Sidebar from "./components/Sidebar";
+// import Navbar from "./components/NavBar";
+// import styled from "styled-components";
 function App() {
   return (
-    <div className="App">
-      {/* <Export/>
-      <TotalContact/> */}
-      {/* <Login/> */}
-      {/* <Signin /> */}
-    <Signup/>
-    </div>
-import Sidebar from "./components/Sidebar";
-import Navbar from "./components/NavBar";
-import styled from "styled-components";
-function App() {
-  return (
-    <Container>
-      <Sidebar/>
+    <>
+      {/* <Sidebar/>
       <Navbar/>
-      <TotalContact/>
-     
-    </Container>
+      <TotalContact/> */}
+        {/* <Signin /> */}
+    <Signup/>
+    </>
   );
 }
 
 export default App;
-const Container=styled.div`
-  margin-top:0px;
-`
